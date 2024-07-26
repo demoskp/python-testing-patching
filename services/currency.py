5690c5b02f208542feec054c5da81a5855d56c58
Demos <>
2024-07-26 11:13:25 +0300

    api changes

--- services/currency.py
+++ services/currency.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 
 
 class FXRateServiceException(Exception):
@@ -14,33 +15,24 @@ class FXRateService:
 
     def historical(self, from_fx, to_fx, date):
         params = {"access_key": self._access_key, "source": from_fx, "currencies": to_fx, "date": date}
-        self._response = requests.get(self.BASE_URL + "/historical", params=params)
-        self._validate_response()
-        return self._extract_fx_rate(from_fx, to_fx)
+        response = requests.get(self.BASE_URL + "/convert", params=params)
+        return self.extract_fx_rate_from_response(response)
 
     def current(self, from_fx, to_fx):
-        params = {"access_key": self._access_key, "source": from_fx, "currencies": to_fx}
-        self._response = requests.get(self.BASE_URL + "/live", params=params)
-        self._validate_response()
-        return self._extract_fx_rate(from_fx, to_fx)
+        params = {"access_key": self._access_key, "from": from_fx, "to": to_fx}
+        response = requests.get(self.BASE_URL + "/convert", params=params)
+        return self.extract_fx_rate_from_response(response)
 
-    def _validate_response(self):
-        if self._response.status_code != 200:
+    @staticmethod
+    def extract_fx_rate_from_response(response: Response):
+        if response.status_code != 200:
             raise FXRateServiceException("Request to fetch fx rate failed")
 
-        data = self._response.json()
+        data = response.json()
         success = data.get("success")
 
         if not success:
             error_info = data.get('error').get('info')
             raise FXRateServiceException(f"There was an error fetching the fx rate, {error_info}")
 
-        quotes = data.get("quotes")
-
-        if not quotes:
-            raise FXRateServiceException("Not fx rate found")
-
-    def _extract_fx_rate(self, from_fx, to_fx):
-        data = self._response.json()
-        quotes = data.get("quotes")
-        return quotes.get(f"{from_fx}{to_fx}")
+        return data.get("result")
